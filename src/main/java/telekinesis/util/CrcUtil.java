5f5f94fe096c30ae6ca3c453be2d0f4a82e4e146
Martin Schrodt <martin@schrodt.org>
2016-08-14 22:02:20 +0200

    clean CrcUtil

--- src/main/java/telekinesis/util/CrcUtil.java
+++ src/main/java/telekinesis/util/CrcUtil.java
@@ -3,30 +3,36 @@ package telekinesis.util;
 import com.google.protobuf.ByteString;
 import io.netty.buffer.ByteBuf;
 
+import java.nio.ByteBuffer;
 import java.util.zip.CRC32;
 
 public class CrcUtil {
 
-    public static int crc32(ByteString in) {
+    public static int crc32(ByteBuffer buf) {
         CRC32 crc32 = new CRC32();
-        crc32.update(in.asReadOnlyByteBuffer());
+        crc32.update(buf);
         return (int) crc32.getValue();
+    }
+
+    public static int crc16(ByteBuffer in) {
+        int c = crc32(in);
+        return ((c >>> 16) ^ c) & 0xffff;
+    }
+
+    public static int crc32(ByteString in) {
+        return crc32(in.asReadOnlyByteBuffer());
     }
 
     public static int crc16(ByteString in) {
-        int c = crc32(in);
-        return ((c >>> 16) ^ c) & 0xffff;
+        return crc16(in.asReadOnlyByteBuffer());
     }
 
     public static int crc32(ByteBuf in) {
-        CRC32 crc32 = new CRC32();
-        crc32.update(in.nioBuffer());
-        return (int) crc32.getValue();
+        return crc32(in.nioBuffer());
     }
 
     public static int crc16(ByteBuf in) {
-        int c = crc32(in);
-        return ((c >>> 16) ^ c) & 0xffff;
+        return crc16(in.nioBuffer());
     }
 
 }
