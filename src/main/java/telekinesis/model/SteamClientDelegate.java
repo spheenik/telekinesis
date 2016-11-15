0df4b4172bd577db2815f22432195a5f6eef3f1b
Martin Schrodt <martin@schrodt.org>
2016-11-15 19:45:12 +0100

    allow to give flags for writeFile()

--- src/main/java/telekinesis/model/SteamClientDelegate.java
+++ src/main/java/telekinesis/model/SteamClientDelegate.java
@@ -3,6 +3,7 @@ package telekinesis.model;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.stream.Stream;
 
 public interface SteamClientDelegate {
@@ -11,7 +12,7 @@ public interface SteamClientDelegate {
     String getPassword();
 
     Stream<Path> findFile(String pattern) throws IOException;
-    void writeFile(String fileName, Integer dstOffset, ByteBuffer data) throws IOException;
+    void writeFile(String fileName, Integer dstOffset, ByteBuffer data, StandardOpenOption... openOptions) throws IOException;
     ByteBuffer readFile(String fileName, Integer dstOffset, Integer length) throws IOException;
     void deleteFile(String fileName) throws IOException;
 
